bc850a81d8672e56e0d0036a0f1932ef48b12f20
Lorenzo Cauli <lorenzocauli162@gmail.com>
2022-01-14 04:04:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rsbsPLUS-nx
+the forbidden one
+
+Enhanced port of JayFoxRox's (aka jefe) magnum opus RSBS to the Nintendo Switch
---
+++ include/vertex.h
@@ -0,0 +1,10 @@
+#ifndef __VERTEX_H_
+#define __VERTEX_H_
+
+struct Vertex {
+    float position[3];
+    float color[3];
+};
+
+
+#endif
